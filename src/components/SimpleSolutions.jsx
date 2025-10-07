cce2e819fb15eb5c2c325a0a4c5d518395953985
bezawitassaye <beza1221love@gmail.com>
2025-10-08 02:28:14 +0300

    another asset

--- src/components/SimpleSolutions.jsx
+++ src/components/SimpleSolutions.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const SimpleSolutions = () => {
   return (
-    <section>
-        
+    <section id='company' className='relative overflow-hidden pt-20 sm:pt-20 md:pt-30 lg:pt-48 py-12 px-4 sm:px-6
+             md:px-8 lg:py-16  lg:px-30 flex flex-row lg:flex-row items-center justify-center'>
+
+                <div>
+                    
+                </div>
+
     </section>
   )
 }
