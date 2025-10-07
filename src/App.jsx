2193842f0d1991d62d44bf3bfe57697fa784be75
bezawitassaye <beza1221love@gmail.com>
2025-10-07 22:58:16 +0300

    finish hero section animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Aos from 'aos'
+import 'aos/dist/aos.css';
+
 const App = () => {
+  useEffect(()=>{
+    Aos.init({
+      duration:1000,
+      easing:"ease-in-out",
+      once:false,
+      offset:100
+    })
+  },[])
   return (
     <div>
       <Navbar/>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,25 +9,25 @@ const Hero = () => {
     md:px-8 lg:py-16  lg:px-30 flex flex-col lg:flex-row items-center justify-between'>
 
       <div className='flex-1 lg:ml-28 justify-center w-full max-w-xl space-y-6 relative z-20'>
-        <h2 className='text-4xl sm:text-5xl text-gray-900 text-center lg:text-left font-bold text-sty '>
+        <h2 className='text-4xl sm:text-5xl text-gray-900 text-center lg:text-left font-bold text-sty  ' data-aos="fade-right">
           We create <br /> <span className='text-orange-500'>solutions</span> for <br /> your business{""}
           <span className='text-orange-500'>.</span>
         </h2>
 
-        <div className='flex mt-4 justify-center lg:justify-start'>
+        <div className='flex mt-4 justify-center lg:justify-start ' data-aos="fade-right" data-aos-delay="100">
           <p className='text-gray-700 text-xl '>
             Our team keeps eye on emerging trends <br /> and technologies to
             ensure your marketing <br /> campaigns remain cutting-edge
           </p>
         </div>
 
-        <div className='pt-2 flex mt-4 justify-center lg:justify-start'>
+        <div className='pt-2 flex mt-4 justify-center lg:justify-start' data-aos="fade-right" data-aos-delay="100">
 
           <button className='px-4 py-2 sm:px-5 sm:py-3 font-medium   rounded-lg sm:rounded-xl bg-orange-500 text-white  hover:bg-orange-600 transition-colors'>Get Started</button>
 
         </div>
 
-        <div className='pt-2 flex mt-4 justify-center lg:justify-start gap-4 items-center'>
+        <div className='pt-2 flex mt-4 justify-center lg:justify-start gap-4 items-center' data-aos="fade-right" data-aos-delay="100">
 
           <button className="flex items-center justify-center bg-white p-4 rounded-full shadow-lg shadow-orange-500/50 transition">
             <Play className="w-6 h-6 text-orange-500 fill-orange-500" />
@@ -41,15 +41,14 @@ const Hero = () => {
 
       </div>
 
-      <div className='flex-1 mt-12 lg:mt-0 lg:ml-12 relative w-full flex justify-center'>
+      <div className='flex-1 mt-12 lg:mt-0 lg:ml-12 relative w-full flex justify-center' data-aos="zoom-in" data-aos-delay="200">
         <img src={persones} className='' alt="" />
-       
+
       </div>
-         <div></div>
-      <div></div>
- <div className='mt-110 hidden lg:block lg:mr-0 '>
-          <img src={down} alt="" className='w-18' />
-        </div>
+     
+      <div className='mt-110 hidden lg:block lg:mr-0 ' data-aos="fade-left" data-aos-delay="400">
+        <img src={down} alt="" className='w-18' />
+      </div>
 
     </section>
   )
