2f5bee273c504260b69deb5bbb1e322617c53f44
bezawitassaye <beza1221love@gmail.com>
2025-10-07 21:45:37 +0300

    finish navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,58 @@
+import { Play } from 'lucide-react'
+import React from 'react'
+import persones from "../assets/persones.png"
+import down from "../assets/down.png"
+
+const Hero = () => {
+  return (
+    <section className='relative overflow-hidden pt-20 sm:pt-20 md:pt-30 lg:pt-48 py-12 px-4 sm:px-6
+    md:px-8 lg:py-16  lg:px-30 flex flex-col lg:flex-row items-center justify-between'>
+
+      <div className='flex-1 lg:ml-28 justify-center w-full max-w-xl space-y-6 relative z-20'>
+        <h2 className='text-4xl sm:text-5xl text-gray-900 text-center lg:text-left font-bold text-sty '>
+          We create <br /> <span className='text-orange-500'>solutions</span> for <br /> your business{""}
+          <span className='text-orange-500'>.</span>
+        </h2>
+
+        <div className='flex mt-4 justify-center lg:justify-start'>
+          <p className='text-gray-700 text-xl '>
+            Our team keeps eye on emerging trends <br /> and technologies to
+            ensure your marketing <br /> campaigns remain cutting-edge
+          </p>
+        </div>
+
+        <div className='pt-2 flex mt-4 justify-center lg:justify-start'>
+
+          <button className='px-4 py-2 sm:px-5 sm:py-3 font-medium   rounded-lg sm:rounded-xl bg-orange-500 text-white  hover:bg-orange-600 transition-colors'>Get Started</button>
+
+        </div>
+
+        <div className='pt-2 flex mt-4 justify-center lg:justify-start gap-4 items-center'>
+
+          <button className="flex items-center justify-center bg-white p-4 rounded-full shadow-lg shadow-orange-500/50 transition">
+            <Play className="w-6 h-6 text-orange-500 fill-orange-500" />
+          </button>
+
+          <span className='text-gray-900 font-medium'>Explore more</span>
+
+        </div>
+
+
+
+      </div>
+
+      <div className='flex-1 mt-12 lg:mt-0 lg:ml-12 relative w-full flex justify-center'>
+        <img src={persones} className='' alt="" />
+       
+      </div>
+
+      <div></div>
+ <div className='mt-110 hidden lg:block lg:mr-0 '>
+          <img src={down} alt="" className='w-18' />
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,101 @@
+import { Menu, X } from 'lucide-react';
+import React, { useState } from 'react'
+import up from "../assets/up.png"
+
+const navItems = [
+    { name: "Home", href: "#home" },
+    { name: "About", href: "#about" },
+    { name: "Services", href: "#services" },
+    { name: "Contact", href: "#contact" },
+    { name: "Company", href: "#company" }
+]
+
+const Navbar = () => {
+    const [ismenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => setIsMenuOpen(!ismenuOpen);
+
+
+    return (
+        <nav className='fixed w-full  top-0 left-0
+    right-0 z-50'>
+            <div className='max-w-7xl bg-gray-50 mx-auto px-4 sm:px-6 md:px-12
+        lg:px-20 py-3 md:py-4 flex items-center justify-between'>
+                {/*logo*/}
+                <div className='flex items-center text-xl sm:text-2xl md:text-3xl  font-bold text-gray-900'>
+                    <div className='w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center  rounded-full border-4 border-orange-500 text-orange-500 mr-2 '>
+                        P
+                    </div>
+                    <span>
+                        ro <span>
+                            Service
+                        </span>
+                    </span>
+
+                </div>
+                {/* Links */}
+                <ul className='hidden md:flex items-center gap-6 lg:gap-8 text-gray-700 font-medium'>
+                    {navItems.map(({ name, href }) => (
+                        <li key={name}>
+                            <a href={href} className='hover:text-orange-500 cursor-pointer transition-colors'>
+                                {name}
+                            </a>
+
+                        </li>
+                    ))}
+
+                </ul>
+                {/*buttons*/}
+                <div className='hidden md:block'>
+                    <button className='px-4 py-2 sm:px-5 sm:py-3 font-bold   rounded-lg sm:rounded-xl bg-orange-500 text-white  hover:bg-orange-600 transition-colors'>Sign Up</button>
+                </div>
+
+
+                {/* Menu */}
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='p-1 rounded-md focus:outline-none focus:ring-2 focus:orange-400'>
+                        {ismenuOpen ? (
+                            <X className='w-6 h-6 text-gray-700' />
+                        ) : (
+                            <Menu className='w-6 h-6 text-gray-700' />
+                        )
+                        }
+
+                    </button>
+
+                </div>
+            </div>
+
+            {ismenuOpen && (
+                <div className='md:hidden bg-white shadow-lg border-t border-gray-200'>
+                    <div className='px-4 py-3 space-y-3'>
+                        {
+                            navItems.map(({ name, href }) => (
+                                <a key={name}
+                                    href={href}
+                                    className='block py-2 px-4 text-gray-700
+                            hover:bg-gray-100 rounded-lg hover:text-orange-400 transition-colors'
+                                    onClick={() => setIsMenuOpen(false)} >{name}</a>
+                            ))
+                        }
+                        <div className='pt-2'>
+                            <button className='w-full py-2 rounded-lg
+                        bg-orange-400 text-white font-medium
+                        hover:bg-orange-500 transition-colors'>
+                                Sign Up
+
+                            </button>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            )}
+            <div className='ml-20  hidden lg:block  '>
+                <img src={up} alt="" className='w-18 hidden lg:block' />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
